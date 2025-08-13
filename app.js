2401fcbe835a4b13add312b5a1cd5d598ab71ce8
VergilAli <alvarezluisfelipe43@gmail.com>
2025-08-12 21:43:17 -0600

    Agregue funcion capturaAmigo

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-let amigos[];
+let amigos = [];
+
+function capturaAmigo(){
+    const nombre = document.getElementById("amigo").value,trim();
+    
+    if (nombre === ""){
+        alert("Introduce un nombre valido.");
+        return;
+    }
+
+    if (amigos.include(nombre)){
+        alert("Este nombre ya existe");
+        return;
+}
+
+amigos.push(nombre);
+document.getElementById("amigo").value = "";
+}
